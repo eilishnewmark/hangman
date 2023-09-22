bcaec2433663574486d7cc427f8f54568c80723e
eilishnewmark <eilish.newmark@icloud.com>
2023-09-22 15:21:03 +0100

    Improve game usage.

--- gameutils.py
+++ gameutils.py
@@ -1,11 +1,5 @@
 import random
 
-def random_choice(seq):
-    if len(seq) == 1:
-        return seq[0]
-
-    return random.choice(seq)
-
 wordlist = 'telecommunication party flump succulent tension' \
            ' willow broaden aerial astronomical beat'.split()
 
@@ -19,4 +13,60 @@ hangman = [
     '''+---+\n|   O\n|  /|\\\n|  /\n===\n''',
     '''+---+\n|   O\n|  /|\\\n|  / \\\n===\n''',
 ]
+
+def random_choice(seq):
+    if len(seq) == 1:
+        return seq[0]
+    return random.choice(seq)
+
+def load_wordlist_from_file(wordfile):
+    """
+    Reads a list of words from a file on disk.
+    :param wordfile: a string giving the path to the wordlist file
+    :return: a list of strings, one for each word in the file. Whitespace is stripped from each word.
+    """
+    with open(wordfile, 'r') as f:
+        wordlist = f.readlines()
+    wordlist = [word.strip() for word in wordlist]
+    return wordlist
+
+def filter_wordlist_for_difficulty(wordlist, difficulty):
+    """
+    Looks at a list of words and returns a subset of them according to their length and the requested difficulty.
+    :param wordlist: A list of strings (words)
+    :param difficulty: a string ['easy', 'medium', or 'hard'].  Defaults to 'hard' if not either 'easy' or 'medium'.
+    :return: A list of strings with the requested difficulty levels.
+    """
+    difficulty = difficulty.lower()
+
+    if difficulty == "easy":
+        return list(filter(lambda word: 1 <= len(word) <= 5, wordlist))
+    elif difficulty == "medium":
+        return list(filter(lambda word: 6 <= len(word) <= 9, wordlist))
+    elif difficulty == "hard":
+        return list(filter(lambda word: 10 <= len(word) <= 100, wordlist))
+    else:
+        print("Please enter difficulty level as either 'easy', 'medium' or 'hard'")
+        exit()
+
+def get_turn_string(turn):
+    """
+    Format the turn number into a nice header to display to the user.  For turn=2, we return:
+    '================Turn 02================='
+    :param turn: an int for how many turns the user has taken
+    :return:  a nicely formatted string to display
+    """
+    turn_header = f"\n\n{'Turn ' f'{turn:02}' :=^40}"
+    return turn_header
+
+def display_turn_dialog(turn, correct_letters, lives_left, hangman_pic=False):
+    if hangman_pic:
+        print(get_turn_string(turn))
+        print(hangman[len(hangman)-lives_left-1])
+        print(f"Mystery word: {' '.join(correct_letters)}")
+    else:
+        print(f"Mystery word: {' '.join(correct_letters)}")
+
+
+
 
--- main.py
+++ main.py
@@ -1,129 +1,58 @@
-import gameutils  # contains a few useful things to re-use
-
-def load_wordlist_from_file(wordfile):
-    """
-    Reads a list of words from a file on disk.
-    :param wordfile: a string giving the path to the wordlist file
-    :return: a list of strings, one for each word in the file. Whitespace is stripped from each word.
-    """
-
-    # TODO: fill in code here to load and return the list of words  (Task 1)
-
-    with open(wordfile, 'r') as f:
-        wordlist = []
-        for line in f:
-            ind_words = line.strip("\n")
-            wordlist.append(ind_words)
-    return wordlist
-
-def filter_wordlist_for_difficulty(wordlist, difficulty):
-    """
-    Looks at a list of words and returns a subset of them according to their length and the requested difficulty.
-    :param wordlist: A list of strings (words)
-    :param difficulty: a string ['easy', 'medium', or 'hard'].  Defaults to 'hard' if not either 'easy' or 'medium'.
-    :return: A list of strings with the requested difficulty levels.
-    """
-
-    # TODO: fill in code here to return a list of only those strings with correct
-    # length, between min_len & max_len inclusive (Task 2)
-
-    difficulty_list = []
-    for word in wordlist:
-        if difficulty == "easy":
-            if 1 <= len(word) <= 5:
-                difficulty_list.append(word)
-        elif difficulty == "medium":
-            if 6 <= len(word) <= 9:
-                difficulty_list.append(word)
-        else:
-            if 10 <= len(word) <= 100:
-                difficulty_list.append(word)
-    return difficulty_list
+import gameutils 
+import os
 
 def get_wordlist(wordfile, difficulty):
     if wordfile:
-        wordlist = load_wordlist_from_file(wordfile)
+        wordlist = gameutils.load_wordlist_from_file(wordfile)
     else:
-        wordlist = gameutils.wordlist  # TODO: change this to use a better default list (one from gameutils module) (Task 3)
-    return filter_wordlist_for_difficulty(wordlist, difficulty)
+        wordlist = gameutils.wordlist
+    return gameutils.filter_wordlist_for_difficulty(wordlist, difficulty)
 
 previous_guesses = []
+
 def get_user_guess(previous_guesses):
     """
     Prompt the user for their guess.  They must enter a single alphabetic character.  We also check whether
-    they've already entered their guess before, and warn them if they have, printing:
-
-    "You already guessed that (X times now!)"  (where X is the number of times they've guessed in total)
-
+    they've already entered their guess before, and warn them.
     Only a guess that is new and is a single alphabetic is returned, and we keep asking until the user enters one.
 
-    :param previous_guesses: a list of strings- one for each guess that has previously been made (updated here too).
-    :return: the guess that meets all criteria, made lowercase for consistency.
+    :param previous_guesses: a list of strings (updated here too).
+    :return: the guess that meets all criteria.
     """
-    while True:
-        guess = input('What letter would you like to guess? (a-z): ')
-
-        # TODO: make sure the user enters a valid guess, so roughly the following steps...  (Task 4)
-        # then check it hasn't been entered before
-        # if it has, print the warning and make them guess again)
-        if guess in previous_guesses:
-            no_of_guesses = previous_guesses.count(guess)
-            print(f"You already guessed that {no_of_guesses} times now!")
-        # check the guess is a single alphabetic character, and...
-        # ...if so, add it to the record of previous guesses
-        # - if it's a new guess, success - we make it lower case for consistency and return it
-        elif (len(guess) != 1) or (guess.isalpha() is False):
+    checking_guess = True
+    while checking_guess:
+        if len(previous_guesses) == 0:
+            guess = input('\nGuess a letter (a-z): ')
+        else: 
+            guess = input('\nGuess another letter (a-z): ')
+        
+        if len(guess) != 1 or not guess.isalpha():
             print("Your guess must be a single letter - try again!")
+        elif guess in previous_guesses:
+            no_of_guesses = previous_guesses.count(guess)
+            previous_guesses.append(guess.lower())
+            print(f"You've already guessed that! No. of previous {guess} guesses: {no_of_guesses}")
         else:
-            guess.lower()
-            previous_guesses.append(guess)
+            previous_guesses.append(guess.lower())
+            checking_guess = False
             return guess
 
-
-# TODO: need to add a function which can be called like "find_matches(word, letter)", which takes a word and a letter
-#  and returns a list of all the indices that character is found.  For example, find_matches('arab', 'a') should return
-#  [0, 2]...  (Task 5)
-
 def find_matches(word, letter):
-    matches = []
-    for n in range(len(word)):
-        if word.find(letter, n) == n:
-            matches.append(n)
+    matches = [n for n in range(len(word)) if word.find(letter, n) == n]
     return matches
 
-def get_turn_string(turn):
-    """
-    Format the turn number into a nice header to display to the user.  The number should be a two-digit integer, padded
-    with zeros, centred in a field of '=' characters a total of 40 wide.  For example, for turn=2, we return:
 
-    '================Turn 02================='
-
-    :param turn: an int for how many turns the user has taken
-    :return:  a nicely formatted string to display
-    """
-
-    turn_header = f"{'Turn ' f'{turn:02}' :=^40}"  #TODO: need to format better (Task 6)
-    return turn_header
-
-def display_turn_dialog(turn, correct_letters, lives_left):
-    print(get_turn_string(turn))
-    print(gameutils.hangman[len(gameutils.hangman)-lives_left-1])
-    print(f"Mystery word: {' '.join(correct_letters)}")
-
-def word_is_complete(letters):
+def word_is_complete(letters, secret_word):
     """
     Check all the letters in the given list to see whether the word is now complete (i.e. no "_" left)
     :param letters: the list of strings (each a single letter) to check
     :return: True if the word is complete, or otherwise False.
     """
-
-    # TODO: fill in code here (Task 7)
-    if "_" in letters:
+    letters = ''.join(letters)
+    if letters.rstrip().lower() != secret_word:
         return False
-    else:
-        return True
+    return True
 
-# The main driver function.  No need to change anything in this function (just read it to understand it though!)
 def play(wordfile=None, difficulty='hard'):
     """
     Play one round of hangman.
@@ -143,27 +72,35 @@ def play(wordfile=None, difficulty='hard'):
     guess_history = []
     turns_taken = 0
     lives_left = 7
+
     while lives_left:
         turns_taken += 1
-
         # display current game state to the user
-        display_turn_dialog(turns_taken, letters, lives_left)
-
+        gameutils.display_turn_dialog(turns_taken, letters, lives_left, hangman_pic=True)
         # get a valid guess from the user
         guess = get_user_guess(guess_history)
-
         # check their guess against the secret word and act accordingly...
         inds = find_matches(secret_word, guess)
         if not inds:
-            print("Incorrect - you lose a life!")
             lives_left -= 1
+            print(f"\nIncorrect - you lose a life!\nYou have {lives_left} lives left.")
         else:
-            print("Correct - that letter is in the secret word!")
+            print("Correct - that letter is in the secret word!\n")
             for i in inds:
                 letters[i] = guess
-            if word_is_complete(letters):
-                print(f"Congratulations, you got the word - '{secret_word}'!")
-                return
+            if word_is_complete(letters, secret_word):
+                    print(f"Congratulations, you got the word - '{secret_word}'!")
+                    return
+            gameutils.display_turn_dialog(turns_taken, letters, lives_left, hangman_pic=False)
+            guess_whole_word = input("Do you want to guess the entire word? Enter Y or N: \n")
+            if guess_whole_word == "Y":
+                guessed_letters = input("Enter your guess: ")
+                if word_is_complete(guessed_letters, secret_word):
+                    print(f"Congratulations, you got the word - '{secret_word}'!")
+                    return
+                else:
+                    lives_left -= 1
+                    print(f"\nIncorrect - you lose a life!\nYou have {lives_left} lives left.")
 
     # if we reach this point, the user failed, so tell them the word
     print(f"{' Game Over ':*^40}")
@@ -171,6 +108,5 @@ def play(wordfile=None, difficulty='hard'):
     print("Too bad - you didn't get the word :(")
     print(f"The word was '{secret_word}'")
 
-
 # finally, run the game with some test parameters!
-play(wordfile='wordlist.txt', difficulty='difficult')
+play(wordfile='wordlist.txt', difficulty='hard')
